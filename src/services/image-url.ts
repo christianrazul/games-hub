13b431b9dd2c9bea87e64f9c296b561c4d612502
Rudolph Christian Razul <razulchristian@gmail.com>
2024-03-13 02:51:41 +0800

    Handle Games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -4,9 +4,10 @@
 
 // NORMAL: https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
 // TARGET: https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+import noImage from '../assets/no-image-placeholder.webp';
 
 const getCroppedImageURL = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
 
   const target = 'media/';
   const index = url.indexOf(target) + target.length;

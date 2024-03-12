835640e5add7ebe03f3a09d077ce98eae7a468ad
Rudolph Christian Razul <razulchristian@gmail.com>
2024-03-12 14:15:22 +0800

    Update steps in image-url.ts

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,13 @@
+// first get the URL of the background images
+// second slice the URL so that we can add the cropper parameters to it
+// third return cropped URL
+
+// NORMAL: https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+// TARGET: https://media.rawg.io/media/crop/600/400/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg
+
 const getCroppedImageURL = (url: string) => {
   const target = 'media/';
-  // get the index up until the last character of 'media/'
   const index = url.indexOf(target) + target.length;
-  // slice from beginning of URL until index and add the cropped parameters, then slice URL starting from index until end and add it
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 };
 
